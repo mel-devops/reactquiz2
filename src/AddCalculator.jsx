5b5fcf125b09338dc1c33e63fc47803401c62add
Melissa Schaefer <mschaefer@xpanxion.com>
2023-05-30 10:48:26 -0400

    update

--- src/AddCalculator.jsx
+++ src/AddCalculator.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import { useState } from "react";
+
 
 export const AddCalculator = () => {
+    let [counter, setTotal]= useState("0")
+
+    const onHandleClick = () => {
+        setTotal(++counter);
+    
+        counter(setTotal + " " + counter);
+      };
+
+     
+
 
     return (
         <>
-            <button>Add 1</button>
-            <button>Add 2</button>
-            <button>Add 3</button>
+            <button oClick={() => onHandleClick()}>Add 1</button>
+            <button onClick={() => onHandleClick()}>Add 2</button>
+            <button onClick={() => OnHandleClick()}>Add 3</button>
             <br></br>
-            Total: 
+            Total:  
         </>
     )
 }
